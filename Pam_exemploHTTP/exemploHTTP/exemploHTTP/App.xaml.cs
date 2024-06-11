f058384582426da3a9b88c5a2a5e2e1f34ef79b1
Rique_Pedro <pedroriqueetec3@gmail.com>
2024-06-11 12:38:45 -0300

    Terminado posts

---
+++ Pam_exemploHTTP/exemploHTTP/exemploHTTP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace exemploHTTP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
